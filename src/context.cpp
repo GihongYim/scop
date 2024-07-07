9d7c7768c25075ee1d759f456dc3dc2b94128cb7
Gihong Yim <gyim@c2r8s1.42seoul.kr>
2024-07-07 18:22:55 +0900

    add texture

--- src/context.cpp
+++ src/context.cpp
@@ -10,28 +10,54 @@ ContextUPtr Context::Create() {
 
 bool Context::Init() {
 
+	// float vertices[] = {
+	// 	0.5f, 0.5f, 0.0f, // top right
+	// 	0.5f, -0.5f, 0.0f, // bottom right
+	// 	-0.5f, -0.5f, 0.0f, // bottom left
+	// 	-0.5f, 0.5f, 0.0f, // top left
+	// };
+	// uint32_t indices[] = { // note that we start from 0!
+	// 	0, 1, 3, // first triangle
+	// 	1, 2, 3, // second triangle
+	// };
+
+	// m_vertexLayout = VertexLayout::Create();
+	// m_vertexBuffer = Buffer::CreateWithData(GL_ARRAY_BUFFER, GL_STATIC_DRAW,
+	// 	vertices, sizeof(float) * 12);
+
+	// m_vertexLayout->SetAttrib(0, 3, GL_FLOAT, GL_FALSE, sizeof(float) * 3, 0);
+
+	// m_indexBuffer = Buffer::CreateWithData(GL_ELEMENT_ARRAY_BUFFER, GL_STATIC_DRAW,
+	// 	indices, sizeof(uint32_t) * 6);
+
+
 	float vertices[] = {
-		0.5f, 0.5f, 0.0f, // top right
-		0.5f, -0.5f, 0.0f, // bottom right
-		-0.5f, -0.5f, 0.0f, // bottom left
-		-0.5f, 0.5f, 0.0f, // top left
-	};
-	uint32_t indices[] = { // note that we start from 0!
-		0, 1, 3, // first triangle
-		1, 2, 3, // second triangle
+		0.5f, 0.5f, 0.0f, 1.0f, 0.0f, 0.0f, 1.0f, 1.0f,
+		0.5f, -0.5f, 0.0f, 0.0f, 1.0f, 0.0f, 1.0f, 0.0f,
+		-0.5f, -0.5f, 0.0f, 0.0f, 0.0f, 1.0f, 0.0f, 0.0f,
+		-0.5f, 0.5f, 0.0f, 1.0f, 1.0f, 0.0f, 0.0f, 1.0f,
 	};
 
 	m_vertexLayout = VertexLayout::Create();
-	m_vertexBuffer = Buffer::CreateWithData(GL_ARRAY_BUFFER, GL_STATIC_DRAW,
-		vertices, sizeof(float) * 12);
+	m_vertexBuffer = Buffer::CreateWithData(
+		GL_ARRAY_BUFFER, GL_STATIC_DRAW,
+		vertices, sizeof(float) * 32);
 
-	m_vertexLayout->SetAttrib(0, 3, GL_FLOAT, GL_FALSE, sizeof(float) * 3, 0);
+	m_vertexLayout->SetAttrib(0, 3, GL_FLOAT, GL_FALSE,
+		sizeof(float) * 8, 0);
+	m_vertexLayout->SetAttrib(1, 3, GL_FLOAT, GL_FALSE,
+		sizeof(float) * 8, sizeof(float) * 3);
+	m_vertexLayout->SetAttrib(2, 2, GL_FLOAT, GL_FALSE,
+		sizeof(float) * 8, sizeof(float) * 6);
+	
 
-	m_indexBuffer = Buffer::CreateWithData(GL_ELEMENT_ARRAY_BUFFER, GL_STATIC_DRAW,
-		indices, sizeof(uint32_t) * 6);
+	
+	// ShaderPtr vertShader = Shader::CreateFromFile("./shader/simple.vs", GL_VERTEX_SHADER);
+	// ShaderPtr fragShader = Shader::CreateFromFile("./shader/simple.fs", GL_FRAGMENT_SHADER);
 
-	ShaderPtr vertShader = Shader::CreateFromFile("./shader/simple.vs", GL_VERTEX_SHADER);
-	ShaderPtr fragShader = Shader::CreateFromFile("./shader/simple.fs", GL_FRAGMENT_SHADER);
+	ShaderPtr vertShader = Shader::CreateFromFile("./shader/texture.vs", GL_VERTEX_SHADER);
+	ShaderPtr fragShader = Shader::CreateFromFile("./shader/texture.fs", GL_FRAGMENT_SHADER);
+
 	if (!vertShader || !fragShader)
 		return false;
 	SPDLOG_INFO("vertex shader id: {}", vertShader->Get());
@@ -51,7 +77,19 @@ bool Context::Init() {
 	auto image = Image::Load("./image/container.jpeg");
 	if (!image)
 		return false;
-	SPDLOG_INFO("image: {}x{}, {} channels", image->GetWidth(), image->GetHeight(), image->GetChannelCount());
+	SPDLOG_INFO("image: {}x{}, {} channels",
+		image->GetWidth(), image->GetHeight(), image->GetChannelCount());
+	
+	glGenTextures(1, &m_texture);
+	glBindTexture(GL_TEXTURE_2D, m_texture);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+
+	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB,
+	    image->GetWidth(), image->GetHeight(), 0,
+	    GL_RGB, GL_UNSIGNED_BYTE, image->GetData());
 	return true;
 }
 
