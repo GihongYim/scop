cc94576ddde13f6fdf9e0b546df827af0206bc0c
GihongYim <56685220+GihongYim@users.noreply.github.com>
2025-02-02 10:21:28 +0900

    lecture lighting

--- src/buffer.cpp
+++ src/buffer.cpp
@@ -1,8 +1,6 @@
 #include "buffer.h"
 
-BufferUPtr Buffer::CreateWithData(
-	uint32_t bufferType, uint32_t usage
-	,const void* data, size_t dataSize) {
+BufferUPtr Buffer::CreateWithData(uint32_t bufferType, uint32_t usage, const void* data, size_t dataSize) {
 	auto buffer = BufferUPtr(new Buffer());
 	if (!buffer->Init(bufferType, usage, data, dataSize))
 		return nullptr;
